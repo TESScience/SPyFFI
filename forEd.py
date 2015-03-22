ba4105268ceda6f2b1298a92bf8cf35c7b959ca6
Zach Berta-Thompson <zkbt@mit.edu>
2015-03-22 00:42:41 -0400

    Add batch submission script.

---
+++ forEd.py
@@ -0,0 +1,16 @@
+#!/corscorpii/d1/zkbt/Ureka/variants/common/bin/python
+#SBATCH --share
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition RegNodes,HyperNodes
+#SBATCH --output=/corscorpii/d1/zkbt/TESS/output.log
+#SBATCH	--time=48:00:00
+#SBATCH --job-name=SPyFFI
+
+import sys
+import numpy as np
+from SPyFFI import Observation
+
+magnitude = np.float(sys.argv[1])
+o = Observation.TestPattern(magnitudes=[magnitude], subarray=100, n=10)
+o.expose()
