Fix bug in PSF rotation (ensure Cartographer returns thetas always > 0).

Cartographer.py
@@ -171,7 +171,7 @@ class focalxy(position):
     @property
     def focalrtheta(self):
         radius = np.sqrt(self.x**2 + self.y**2)
-        theta = np.arctan2(self.y, self.x)
+        theta = (np.arctan2(self.y, self.x) + 2*np.pi) % (2*np.pi)
         return focalrtheta(radius, theta, self.cartographer)
 
     @property

forEd.py
@@ -12,5 +12,5 @@ import numpy as np
 from SPyFFI import Observation
 
 magnitude = np.float(sys.argv[1])
-o = Observation.TestPattern(magnitudes=[magnitude], subarray=100, n=10)
-o.expose()
+o = Observation.TestPattern(magnitudes=[magnitude], subarray=4096, n=10, random=True)
+o.expose(jitter=True)