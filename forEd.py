cdd2f6612760deb70496bab1f957bab71cc696e7
Zach Berta-Thompson <zkbt@mit.edu>
2015-03-22 17:00:33 -0400

    Hmmm.

--- forEd.py
+++ forEd.py
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition RegNodes,HyperNodes
-#SBATCH --output=/corscorpii/d1/zkbt/TESS/output.log
+#SBATCH --output=/corscorpii/d1/zkbt/tess/output.log
 #SBATCH	--time=48:00:00
 #SBATCH --job-name=SPyFFI
 
@@ -11,6 +11,6 @@ import sys
 import numpy as np
 from SPyFFI import Observation
 
-magnitude = np.float(sys.argv[1])
+magnitude =10.0; np.float(sys.argv[1])
 o = Observation.TestPattern(magnitudes=[magnitude], subarray=100, n=10)
 o.expose()
