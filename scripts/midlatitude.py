2e4d0d1d37313a6d534dc2c71cdec82b20fe04ad
Zach Berta-Thompson <zkbt@mit.edu>
2015-09-27 14:06:34 -0400

    update label for midlatitude

--- scripts/midlatitude.py
+++ scripts/midlatitude.py
@@ -2,7 +2,7 @@
 # create an observation centered at the north ecliptic pole (midlatitude)
 
 import SPyFFI.Observation
-o = SPyFFI.Observation.SkyFFI(ra=270.00000, dec=66.56071, label='20150914')
+o = SPyFFI.Observation.SkyFFI(ra=270.00000, dec=66.56071, label='20150927')
 ccds = o.camera.ccds*1
 for c in ccds:
     o.camera.ccds = [c]
