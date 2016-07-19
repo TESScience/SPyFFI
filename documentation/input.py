bcaf2d3d338ceccbff17d0f598e3dbc3c028c25e
John Doty <jpd@noqsi.com>
2016-07-19 16:09:59 -0600

    A few more keywords

--- documentation/input.py
+++ documentation/input.py
@@ -4,6 +4,9 @@ documented_keywords = {
                     'lckw': '???'},
     'observation': {'cadencestodo': '???',
                     'collate': '???'},
-    'camera' : {},
+    'camera' : {'cadence': 'Do not use, use observation.cadencestodo',
+    			'ra': 'Right Ascension of field center',
+    			'dec': 'Declination of field center',
+    			'testpattern': 'Broken, do not use'},
     'expose' : {}
 }
