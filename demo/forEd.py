9f4b042082fd176e671ccee8d31b7dd12bf9ae27
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-08-01 15:40:53 -0400

    Getting rid of spurious demo directory

--- demo/forEd.py
+++
@@ -1,8 +0,0 @@
-import sys
-import numpy as np
-from SPyFFI import Observation
-
-magnitude =10.0; np.float(sys.argv[1])
-o = Observation.TestPattern(magnitudes=[magnitude], subarray=100, n=10)
-o.expose()
-
