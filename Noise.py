84257051fd080c59238bc1c471633464ff4bde18
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-08-01 12:14:45 -0400

    Reformatting, using pkgutil.get_data

--- Noise.py
+++ Noise.py
@@ -1,70 +1,79 @@
-'''Calculate the expected photometric precision for TESS.
+"""Calculate the expected photometric precision for TESS.
 
-(translated from Josh Winn's IDL TESS signal-to-noise calculator on the TESS wiki and updated to include calculations published with Peter Sullivan's simulation paper)'''
+(translated from Josh Winn's IDL TESS signal-to-noise calculator
+ on the TESS wiki and updated to include calculations
+ published with Peter Sullivan's simulation paper)."""
+import pkgutil
 
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.io.ascii
 import scipy.interpolate
 
-try:
-    import Cartographer
-    # create a cartographer for managing conversions between ecliptic and galactic coordinates
-    carto = Cartographer.Cartographer()
-except:
-    pass
+import Cartographer
 
 
-import os
-relationsdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'relations'))
+# create a cartographer for managing conversions between ecliptic and galactic coordinates
+carto = Cartographer.Cartographer()
 
 # create an interpolator to estimate the best number of pixels in a photometric aperture
-optimalpixelsdata = astropy.io.ascii.read(relationsdir + '/optimalnumberofpixels.txt')
-optimalpixelsinterpolator = scipy.interpolate.interp1d(optimalpixelsdata['tmag'], optimalpixelsdata['npix'], kind='linear', bounds_error=True)
+optimalpixelsdata = astropy.io.ascii.read(pkgutil.get_data(__name__, 'relations/optimalnumberofpixels.txt'))
+optimalpixelsinterpolator = scipy.interpolate.interp1d(optimalpixelsdata['tmag'], optimalpixelsdata['npix'],
+                                                       kind='linear', bounds_error=True)
+
+
 def optimal_npix(imag):
-    '''Return the average best number of pixels to use in a photometric aperture, given a Cousins I magnitude.'''
+    """Return the average best number of pixels to use in a photometric aperture, given a Cousins I magnitude."""
     return optimalpixelsinterpolator(imag)
 
+
 # create an interpolator to estimate the average enclosed flux fraction for a aperture size
-encloseddata = astropy.io.ascii.read(relationsdir + '/enclosedfractionofflux.txt')
+encloseddata = astropy.io.ascii.read(pkgutil.get_data(__name__, 'relations/enclosedfractionofflux.txt'))
 enclosedinterpolator = scipy.interpolate.interp1d(encloseddata['npix'], encloseddata['fraction'])
+
+
 def enclosed_fraction(npix):
-    '''Return the average fraction of enclosed energy in a given number of pixels.'''
+    """Return the average fraction of enclosed energy in a given number of pixels."""
     return enclosedinterpolator(npix)
 
-# create an interpolator to estimate the TESS magnitude for a star, given is Cousin I magnitude and effective temperature
-fluxesdata = astropy.io.ascii.read(relationsdir + '/fluxes.txt')
-fluxesinterpolator = scipy.interpolate.interp1d(fluxesdata['teff'], fluxesdata['icminust']/1000.0)
+
+# create an interpolator to estimate the TESS magnitude for a star,
+# given is Cousin I magnitude and effective temperature
+fluxesdata = astropy.io.ascii.read(pkgutil.get_data(__name__, 'relations/fluxes.txt'))
+fluxesinterpolator = scipy.interpolate.interp1d(fluxesdata['teff'], fluxesdata['icminust'] / 1000.0)
+
+
 def IctoT(Icmag, teff=5000.0):
-    '''Return the TESS magnitude of a star, given its Cousin I magnitude and effective temperature.'''
+    """Return the TESS magnitude of a star, given its Cousin I magnitude and effective temperature."""
     tmag = Icmag - fluxesinterpolator(teff)
     return tmag
 
-def noise(imag=10.0, exptime=1800.0, teff=5000.0, \
-            elon=0.0, elat=30.0, glon=None, glat=None, ra=None, dec=None, \
-            subexptime=2.0, npix_aper=4, frac_aper=0.76, e_pix_ro=10.0, \
-            effective_area = 73.0, pix_scale=21.1, sys_limit=60.0, \
-            verbose=False):
-    '''Calculate noise, given input Ic magnitude, returing the fractional rms (= 1/snr)
 
-        ; mandatory inputs
-        ;
-           imag, $                          ; apparent mag in Cousins I band
-        ;
-        ; optional inputs
-        ;
-           exptime                          ; total exposure time in seconds
-           teff                             ; effective temperature in Kelvins
-           elon, elat                       ; ecliptic coordinates in degrees
-           subexptime                       ; subexposure time (n_exp = exptime/subexptime)
-           npix_aper                        ; number of pixels in photometric aperture
-           frac_aper                        ; fraction of flux enclosed in photometric aperture
-           e_pix_ro                         ; rms in no. photons/pixel from readout noise
-           effective_area                        ; geometric collecting area
-           pix_scale                        ; arcsec per pixel
-           sys_limit                        ; minimum uncertainty in 1 hr of data, in ppm
-           verbose                          ; request verbose output
-    '''
+def noise(imag=10.0, exptime=1800.0, teff=5000.0,
+          elon=0.0, elat=30.0, glon=None, glat=None, ra=None, dec=None,
+          subexptime=2.0, npix_aper=4, frac_aper=0.76, e_pix_ro=10.0,
+          effective_area=73.0, pix_scale=21.1, sys_limit=60.0,
+          verbose=False):
+    """Calculate noise, given input Ic magnitude, returing the fractional rms (= 1/snr)
+
+        Mandatory inputs
+
+           imag, $                           apparent mag in Cousins I band
+
+        Optional inputs
+
+           exptime                           total exposure time in seconds
+           teff                              effective temperature in Kelvins
+           elon, elat                        ecliptic coordinates in degrees
+           subexptime                        subexposure time (n_exp = exptime/subexptime)
+           npix_aper                         number of pixels in photometric aperture
+           frac_aper                         fraction of flux enclosed in photometric aperture
+           e_pix_ro                          rms in no. photons/pixel from readout noise
+           effective_area                    geometric collecting area
+           pix_scale                         arcsec per pixel
+           sys_limit                         minimum uncertainty in 1 hr of data, in ppm
+           verbose                           request verbose output
+    """
 
     # convert from Cousins I to TESS magnitude
     tmag = IctoT(imag, teff=teff)
@@ -76,16 +85,16 @@ def noise(imag=10.0, exptime=1800.0, teff=5000.0, \
     frac_aper = enclosed_fraction(npix_aper)
 
     # solid area of a pixel
-    omega_pix = pix_scale**2.
+    omega_pix = pix_scale ** 2.
 
     # how many subexposures composed this one exposure?
-    n_exposures = exptime/subexptime
+    n_exposures = exptime / subexptime
 
     # the TESS zeropint
     tmag0 = 1.514e6
 
     # photoelectrons from the star
-    e_star = 10.0**(-0.4*tmag) * tmag0 * effective_area * exptime * frac_aper
+    e_star = 10.0 ** (-0.4 * tmag) * tmag0 * effective_area * exptime * frac_aper
 
     if ra is not None and dec is not None:
         elon, elat = carto.point(ra, dec, 'celestial').ecliptic.tuple
@@ -110,9 +119,9 @@ def noise(imag=10.0, exptime=1800.0, teff=5000.0, \
         print 'e_star = ', e_star
 
     # photoelectrons/pixel from zodiacal light
-    dlat = (np.abs(elat)-90.)/90.
-    vmag_zodi = 23.345 - 1.148*dlat**2.
-    e_pix_zodi = 10.0**(-0.4*(vmag_zodi-22.8)) * 2.39e-3 * effective_area * omega_pix * exptime
+    dlat = (np.abs(elat) - 90.) / 90.
+    vmag_zodi = 23.345 - 1.148 * dlat ** 2.
+    e_pix_zodi = 10.0 ** (-0.4 * (vmag_zodi - 22.8)) * 2.39e-3 * effective_area * omega_pix * exptime
 
     if verbose:
         print 'vmag_zodi = ', vmag_zodi
@@ -127,28 +136,27 @@ def noise(imag=10.0, exptime=1800.0, teff=5000.0, \
     glon = np.array([glon])
     glat = np.array([glat])
 
-
     if verbose:
         print 'glon, glat = ', glon, glat
 
-    dlat = np.abs(glat)/40.0
+    dlat = np.abs(glat) / 40.0
     dlon = glon
     q = (dlon > 180.)
-    dlon[q] = 360.-dlon[q]
-    dlon = np.abs(dlon)/180.0
+    dlon[q] = 360. - dlon[q]
+    dlon = np.abs(dlon) / 180.0
     p = [18.9733, 8.833, 4.007, 0.805]
-    imag_bgstars = p[0] + p[1]*dlat + p[2]*dlon**(p[3])
-    e_pix_bgstars = 10.0**(-0.4*imag_bgstars) * 1.7e6 * effective_area * omega_pix * exptime
+    imag_bgstars = p[0] + p[1] * dlat + p[2] * dlon ** (p[3])
+    e_pix_bgstars = 10.0 ** (-0.4 * imag_bgstars) * 1.7e6 * effective_area * omega_pix * exptime
 
     if verbose:
         print 'imag_bgstars = ', imag_bgstars
         print 'e_pix_bgstars = ', e_pix_bgstars
 
     noise_star = np.sqrt(e_star) / e_star
-    noise_sky  = np.sqrt(npix_aper*(e_pix_zodi + e_pix_bgstars)) / e_star
-    noise_ro   = np.sqrt(npix_aper*n_exposures)*e_pix_ro / e_star
-    noise_sys  = 0.0*noise_star + sys_limit/1e6/np.sqrt(exptime/3600.)
-    noise = np.sqrt( noise_star**2. + noise_sky**2. + noise_ro**2. + noise_sys**2. )
+    noise_sky = np.sqrt(npix_aper * (e_pix_zodi + e_pix_bgstars)) / e_star
+    noise_ro = np.sqrt(npix_aper * n_exposures) * e_pix_ro / e_star
+    noise_sys = 0.0 * noise_star + sys_limit / 1e6 / np.sqrt(exptime / 3600.)
+    noise = np.sqrt(noise_star ** 2. + noise_sky ** 2. + noise_ro ** 2. + noise_sys ** 2.)
 
     '''if verbose:
         print 'noise_star [ppm] = ', noise_star*1e6
@@ -159,36 +167,38 @@ def noise(imag=10.0, exptime=1800.0, teff=5000.0, \
         '''
     return noise
 
-def demo(span=27.4, period=12.345678, mean=17, amplitude=1.0):
-    '''Demonstration of the TESS noise calculator, on a faint and highly-variable star.'''
 
+def demo(span=27.4, period=12.345678, mean=17, amplitude=1.0):
+    """Demonstration of the TESS noise calculator, on a faint and highly-variable star."""
 
     # create times at a half hour spacing
-    t = np.arange(0, span, 0.5/24.0)
+    t = np.arange(0, span, 0.5 / 24.0)
     n = len(t)
 
     # create a (perfectly smooth) noiseless model
-    noiselessmodel = mean + amplitude*np.sin(2*np.pi*t/period)
+    noiselessmodel = mean + amplitude * np.sin(2 * np.pi * t / period)
 
     # calculate the per-point photometric uncertainty for each point of that model
+    # noinspection PyTypeChecker
     perpointuncertainty = noise(imag=noiselessmodel)
 
     # create one random realization of the noise
-    noiserealization = np.random.normal(0,1,n)*perpointuncertainty
+    noiserealization = np.random.normal(0, 1, n) * perpointuncertainty
 
     # simulate measurements
     simulated = noiselessmodel + noiserealization
 
     # create plot showing the demonstration
     plt.ion()
-    plt.figure('demonstration', figsize=(10,3), dpi=200)
+    plt.figure('demonstration', figsize=(10, 3), dpi=200)
     plt.cla()
     # plot the simulated measurements
     plt.errorbar(t, simulated, perpointuncertainty, marker='o', elinewidth=2, linewidth=0, color='black', alpha=0.5)
     # plot the noiseless model
-    plt.plot(t,noiselessmodel, color='green', linewidth=2, alpha=0.5)
+    plt.plot(t, noiselessmodel, color='green', linewidth=2, alpha=0.5)
     # clean up the look of the plot
-    plt.ylim(mean + amplitude + np.max(perpointuncertainty)*5, mean - amplitude - np.min(perpointuncertainty)*5)
+    # noinspection PyTypeChecker
+    plt.ylim(mean + amplitude + np.max(perpointuncertainty) * 5, mean - amplitude - np.min(perpointuncertainty) * 5)
     plt.xlim(np.min(t), np.max(t))
     plt.xlabel('Time (in days)')
     plt.ylabel('Flux (magnitudes)')
--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-from distutils.core import setup, Extension
-import numpy.distutils.misc_util
+from setuptools import setup
 
 VERSION = '0.1.0'
 
 setup(
     name='SPyFFI',
     version=VERSION,
-    package_dir={'spyffi': '.'},
-    packages=['spyffi'],
+    package_dir={
+        'SPyFFI': '.',
+        'SPyFFI.debug': './debug',
+        'SPyFFI.documentation': './documentation',
+    },
+    packages=['SPyFFI', 'SPyFFI.debug', 'SPyFFI.documentation'],
+    package_data={
+        'SPyFFI': [
+            'data/*.txt',       # Light Curve tables
+            'relations/*.txt',  # Flux data
+        ],
+    },
     description="Spiffy Python for Full Frame Images (tools for simulating TESS images, calibrations, and light curves)",
     author='Zach Berta-Thompson',
     author_email='zkbt@mit.edu',
@@ -15,11 +24,11 @@ setup(
     # TODO: Version bump this when zachopy has a new release
     install_requires=['zachopy==0.1.0', 'matplotlib', 'numpy', 'astropy', 'astroquery'],
     dependency_links=['git+https://github.com/zkbt/zachopy.git#egg=zachopy-0.1.0'],
-    ext_modules=[Extension("cosmical_realistic._cosmical", ["cosmical_realistic/_cosmical.c",
-                                                            "cosmical_realistic/cosmical.c",
-                                                            "cosmical_realistic/twister.c",
-                                                            "cosmical_realistic/seed_tw_ran.c"],
-                           include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())],
+    # ext_modules=[Extension("cosmical_realistic._cosmical", ["cosmical_realistic/_cosmical.c",
+    #                                                         "cosmical_realistic/cosmical.c",
+    #                                                         "cosmical_realistic/twister.c",
+    #                                                         "cosmical_realistic/seed_tw_ran.c"],
+    #                        include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())],
     # Uncomment this if there's a tagged release that's the same as VERSION, and SPyFFI is publicly released
     # download_url = 'https://github.com/zkbt/SPyFFI/tarball/{}'.format(VERSION),
 )
