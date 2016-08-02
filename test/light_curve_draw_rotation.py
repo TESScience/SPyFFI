47a88da1c56acd93f23b99a8ae240cc7d149dd23
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-08-02 18:31:57 -0400

    Switching light curve tests to use JSONs

--- test/light_curve_draw_rotation.py
+++ test/light_curve_draw_rotation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 # noinspection PyUnresolvedReferences
 from SPyFFI.Lightcurve import draw_rotation
 from numpy.random import RandomState
-from pprint import pprint
+import json
 
-
-pprint([[draw_rotation(prng=RandomState(seed)).model(i) for seed in range(10)]
-        for i in [0.0, 0.1, 0.2, 0.3, 0.4]])
+print(json.dumps(
+    [[draw_rotation(prng=RandomState(seed)).model(i) for seed in range(10)]
+     for i in [0.0, 0.1, 0.2, 0.3, 0.4]],
+    indent=4))
--- test/light_curve_draw_transit.py
+++ test/light_curve_draw_transit.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 # noinspection PyUnresolvedReferences
 from SPyFFI.Lightcurve import draw_transit
 from numpy.random import RandomState
-from pprint import pprint
+import json
 
-pprint([[draw_transit(prng=RandomState(seed)).model(i) for seed in range(50)]
-        for i in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]])
+print(json.dumps([[draw_transit(prng=RandomState(seed)).model(i).tolist() for seed in range(50)]
+                  for i in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]],
+                 indent=4))
